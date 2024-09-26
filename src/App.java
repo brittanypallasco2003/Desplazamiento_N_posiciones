f6720dbd273461a3d6fda8cde2877a694cd9f48a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-09-26 17:19:21 -0500

    Desplazamiento circular

---
+++ src/App.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+        int enteros[] = new int[10];
+        System.out.println("Ingrese un número para los desplazamientos: ");
+        int numeroDesplazamientos = entrada.nextInt();
+
+        for (int i = 0; i < enteros.length; i++) {
+            System.out.println("Ingrese valores: ");
+            enteros[i] = entrada.nextInt();
+        }
+
+        // Rescatar números
+        int numeroIndicePartida = 10 - numeroDesplazamientos;
+        int numerosRescatados[] = new int[numeroDesplazamientos];
+        int contador = 0;
+
+        for (int i = numeroIndicePartida; i < enteros.length; i++) {
+            numerosRescatados[contador] = enteros[i];
+            contador++;
+        }
+
+        // Mover números
+        for (int i = enteros.length - 1; i >= numeroDesplazamientos; i--) {
+            enteros[i] = enteros[i - numeroDesplazamientos];
+        }
+
+        // Guardar números rescatados en las primeras posiciones del arreglo
+        for (int i = 0; i < numerosRescatados.length; i++) {
+            enteros[i] = numerosRescatados[i];
+        }
+
+        // Imprimir valores
+        for (int i : enteros) {
+            System.out.println("Elemento: " + i);
+        }
+
+    }
+
+}
